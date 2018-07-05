d50f46c18528018714e38afd9daf919f9490ebb3
elangokpm <31618257+elangokpm@users.noreply.github.com>
2018-07-05 11:26:32 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'PyUtil',
+    version = '1.0.0',
+    url = 'https://github.com/mypackage.git',
+    author = 'Elango K',
+    author_email = 'elango.kpm@gmail.com’,
+    description = 'python utilities for machine learning',
+    packages = find_packages(),    
+    install_requires = ['numpy >= 1.11.1', 'pandas >= 0.23.0', 'seaborn >= 0.8.1],
+)
